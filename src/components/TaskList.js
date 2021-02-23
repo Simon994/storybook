1d87acb8b34af86d3235355edce02c71a09d5822
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-23 15:06:30 +0000

    Add Tasklist’s test states

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,25 +1,55 @@
-import React from 'react'
-import Task from '.Task'
+// src/components/TaskList.js
 
-export default function TaskList ({loading, tasks, onPinTask, onArchiveTask}) {
+import React from 'react';
+
+import Task from './Task';
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
-    onArchiveTask
-  }
+    onArchiveTask,
+  };
 
   if (loading) {
-    return <div className='list-items'>Loading</div>
+    return <div className="list-items">loading</div>;
   }
 
   if (tasks.length === 0) {
-    return <div className='list-items'>empty</div>
+    return <div className="list-items">empty</div>;
   }
 
   return (
-    <div className='list-items'>
-      {tasks.map(task => {
-        <Task key={task.id} task={task} {...events}/>
-      })}
+    <div className="list-items">
+      {tasks.map(task => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
     </div>
-  )
+  );
 }
+
+
+// import React from 'react'
+// import Task from './Task'
+
+// export default function TaskList ({loading, tasks, onPinTask, onArchiveTask}) {
+//   const events = {
+//     onPinTask,
+//     onArchiveTask
+//   }
+
+//   if (loading) {
+//     return <div className='list-items'>Loading</div>
+//   }
+
+//   if (tasks.length === 0) {
+//     return <div className='list-items'>empty</div>
+//   }
+
+//   return (
+//     <div className='list-items'>
+//       {
+//       tasks.map(task => <Task key={task.id} task={task} {...events}/>)
+//       }
+//     </div>
+//   )
+// }
